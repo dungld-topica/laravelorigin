7a09b37f7d2e8599adbad545864cb23a840d7fca
LYDINHDUNG-PC\Ly Dinh Dung <dungld@navigo-tech.com>
2018-07-19 10:16:27 +0700

    baseline laravel origin

---
+++ database/db_laravel_origin/db_laravel_origin.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : Local
+Source Server Version : 50505
+Source Host           : localhost:3306
+Source Database       : db_laravel_origin
+
+Target Server Type    : MYSQL
+Target Server Version : 50505
+File Encoding         : 65001
+
+Date: 2018-07-19 10:15:33
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
